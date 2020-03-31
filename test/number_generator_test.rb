d52e62e6e40857d3c3cc4f01b025d9c3551d48cd
memcmahon <mcmahon.meganelizabeth@gmail.com>
2020-03-31 11:06:08 -0600

    Initial Commit

---
+++ test/number_generator_test.rb
@@ -0,0 +1,7 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/number_generator'
+
+class NumberGeneratorTest < Minitest::Test
+# Using stubs and/or mocks, write the tests to confirm the functionality that exists in ./lib/number_generator.rb
+end
